b46f3e683c6896cde76740cc724b8c2b3ea43c4c
William Bassonville <wbassonville@outlook.fr>
2024-10-26 18:44:29 +0200

    <feat> Added constraint to exercise 3 description.

--- src/main/java/exercises/ex03/ListCalculatorExercise.java
+++ src/main/java/exercises/ex03/ListCalculatorExercise.java
@@ -2,6 +2,8 @@ package main.java.exercises.ex03;
 
 import main.java.common.AbstractExercise;
 
+import java.util.List;
+
 import static main.java.common.CommonUtils.isFrenchLanguage;
 
 public class ListCalculatorExercise extends AbstractExercise {
@@ -12,7 +14,7 @@ public class ListCalculatorExercise extends AbstractExercise {
 
     @Override
     public void run() {
-        // ...
+
     }
 
     @Override
@@ -22,17 +24,21 @@ public class ListCalculatorExercise extends AbstractExercise {
                 Exercice 3 : Wildcards avec bornes supérieures
                 
                 Objectif : Manipuler des listes de différents types numériques
-                - Utiliser ? extends Number pour accepter tous les types numériques
+                - Utiliser <? extends Number> pour accepter tous les types numériques
                 - Calculer la somme et la moyenne des éléments
                 - Démontrer l'utilisation avec différents types de nombres
+                
+                Contrainte : Le constructeur de ListCalculator doit prendre une liste de NumberBox.
                 """
                 : """
                 Exercise 3: Wildcards with Upper Bounds
                 
                 Objective: Manipulate lists of different numeric types
-                - Use ? extends Number to accept all numeric types
+                - Use <? extends Number> to accept all numeric types
                 - Calculate the sum and average of the elements
                 - Demonstrate usage with different types of numbers
+                
+                Constraint: The constructor of ListCalculator must take a list of NumberBox.
                 """;
     }
 }
